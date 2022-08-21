d04f991e70da82266d40d6125a2b2824d8257498
Crizant Lai <crizant3908@gmail.com>
2022-08-21 14:15:09 +0800

    use ConsumerRouterDelegate

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:navigator2_riverpod_example/router/router_delegate.dart';
 
 final activeColorProvider = StateProvider<Color?>((ref) => null);
+
+final routerDelegateProvider =
+    ChangeNotifierProvider((ref) => ColorRouterDelegate(ref));
